6fa02fd54e4d8a46f7e5d982103534d11445ee8b
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2025-01-05 15:25:10 -0300

    Add files via upload

--- src/menu/stream_serializado.rs
+++ src/menu/stream_serializado.rs
@@ -1,17 +1,18 @@
-
-/*! Serializa o conjunto de dados
- dado pelo pela saída de dados
- principal.
-*/
-
+/*! Serializa o conjunto de dados dado pelo pela saída de dados principal. 
+ */
 
 use crate::motor::Primos;
 use std::io::{self, StdoutLock as SL, Write};
 use std::mem;
+use std::process::Child;
+use std::io::Read;
 
-/* auxiliando no desenvolvimento da 
- * aplicação. */
+type Bytes = Vec<u8>;
+
+
+/* Auxiliando no desenvolvimento da aplicação. */
 #[allow(dead_code)]
+#[allow(clippy::suspicious_else_formatting)]
 fn impressao_debug(primo: u64, ordem: usize, total: usize) {
    if ordem % 15 != 0 { 
       if ordem < total-1
@@ -20,23 +21,18 @@ fn impressao_debug(primo: u64, ordem: usize, total: usize) {
          // quebra-de-linha final.
          { println!("{}", primo); }
    } else
-      { println!(""); }
+      { println!(); }
 }
 
-/* pega o número de 64-bits e cospes seus 
- * 8 bytes, sem espaço, sem nada, para 
- * o próximo. São despejados no output via
- * pelo modo BigEnding. */
+/* Pega o número de 64-bits e cospes seus 8 bytes, sem espaço, sem nada, 
+ * para o próximo. São despejados no output via pelo modo BigEnding. */
 fn despeja_inteiro_64bits(numero: u64, saida: &mut SL) {
    let bytes = numero.to_be_bytes();
-   saida.write(&bytes[..]).unwrap(); 
+   let _ = saida.write(&bytes[..]).unwrap(); 
 }
 
-/** 
- pega o conjunto de primos, e despeja todos
- os bytes que forma cada, pela saída padrão
- do dispotivo.
-*/
+/** Pega o conjunto de primos, e despeja todos os bytes que forma cada, 
+ * pela saída padrão do dispotivo. */
 pub fn despeja_bytes(conjunto: Primos) {
    let mut saida = io::stdout().lock();
    for p in conjunto.iter() 
@@ -45,7 +41,7 @@ pub fn despeja_bytes(conjunto: Primos) {
    mem::drop(saida);
 }
 
-type Bytes = Vec<u8>;
+#[allow(clippy::suspicious_else_formatting)]
 fn parse_bytes(mut array: Bytes) -> Vec<u64> {
    let mut a: [u8; 8] = [0; 8];
    let mut lista: Vec<u64> = Vec::new();
@@ -63,12 +59,8 @@ fn parse_bytes(mut array: Bytes) -> Vec<u64> {
    return lista;
 }
 
-use std::process::Child;
-use std::io::Read;
-/* transforma array de bytes lida em
- * inteiros positivos de 64-bits, que 
- * são provavelmente os números primos.
- */
+/* Transforma array de bytes lida em inteiros positivos de 64-bits, que 
+ * são provavelmente os números primos. */
 pub fn colhe_resultado(sp: &mut Child) -> Vec<u64> {
    let mut bytes: Vec<u8>; 
    bytes = Vec::with_capacity(10_000);  
--- src/menu/tipo.rs
+++ src/menu/tipo.rs
@@ -1,14 +1,10 @@
-
-
-/* refatoração: os enumeradores e funções
- * ou métodos referentes a eles serão 
- * deslocados para aqui.
- */
+/* Refatoração: os enumeradores e funções ou métodos referentes a eles 
+ * serão deslocados para aqui. */
 
 use std::str::FromStr;
 use std::time::Duration;
 use std::fmt::Error;
-//use std::ops::RangeInclusive;
+
 
 #[derive(Debug)]
 pub enum Funcao { 
@@ -50,7 +46,7 @@ fn argumento_tempo_valido(arg:&str) -> bool {
    let arg = arg.to_ascii_uppercase();
    let tem_acronimo = {
       arg.contains("min") ||
-      arg.contains("h") || 
+      arg.contains('h') || 
       arg.contains("seg")
    };
    
@@ -82,28 +78,25 @@ fn argumento_tempo_valido(arg:&str) -> bool {
    tem_acronimo
 }
 
-/* string, que contém caractéres, porém
- * não apenas eles, deve ter uma contagem
- * correta. */
+/* String, que contém caractéres, porém não apenas eles, deve ter uma 
+ * contagem correta. */
 fn n_caracteres(string: &str, n: usize) -> bool {
    string.chars()
    .filter(|c| c.is_alphabetic())
    .count() == n
 }
 
-/* verifica se a string têm uma parte númerica
- * representando o tempo demandado, e, se 
- * tem um peso válido, ou seja, uma slice-string
- * valuando aquela parte númerica num total
- * de segundos. */
+/* Verifica se a string têm uma parte númerica representando o tempo 
+ * demandado, e, se tem um peso válido, ou seja, uma slice-string valuando 
+ * aquela parte númerica num total de segundos. */
 fn argumento_valido(arg: &str) -> bool {
    let tem_acronimo = {
       arg.contains("minuto") && n_caracteres(arg, 6)  ||
       arg.contains("hora") && n_caracteres(arg, 4) || 
       arg.contains("segundo") && n_caracteres(arg, 7) ||
-      arg.contains("m") && n_caracteres(arg, 1) ||
-      arg.contains("s") && n_caracteres(arg, 1) ||
-      arg.contains("h") && n_caracteres(arg, 1) ||
+      arg.contains('m') && n_caracteres(arg, 1) ||
+      arg.contains('s') && n_caracteres(arg, 1) ||
+      arg.contains('h') && n_caracteres(arg, 1) ||
       arg.contains("minutos") && n_caracteres(arg, 7)  ||
       arg.contains("horas") && n_caracteres(arg, 5) || 
       arg.contains("segundos") && n_caracteres(arg, 8) ||
@@ -121,7 +114,7 @@ fn argumento_valido(arg: &str) -> bool {
    tem_parte_numerica && tem_acronimo
 }
 
-/// parte do pressuposto de apenas números.
+/// Parte do pressuposto de apenas números.
 fn apenas_numerica(arg:&str) -> bool {
    for alg in arg.chars() {
       if !alg.is_ascii_digit() {
@@ -132,31 +125,34 @@ fn apenas_numerica(arg:&str) -> bool {
    return true;
 }
 
-/** retorna um tipo de enum mais adequado 
- para os dados argumentos passados. */
+/** Retorna um tipo de enum mais adequado para os dados argumentos 
+ * passados. */
+#[allow(clippy::suspicious_else_formatting)]
 pub fn transforma(argumentos:&Vec<String>) -> Argumentos {
    let total = argumentos.len();
-   /* se nenhum argumento for cedido, mostrar opção 'ajuda'.
+
+   /* Se nenhum argumento for cedido, mostrar opção 'ajuda'.
     * incompleta, retorna pedido de ajuda. */
-   if total == 1 {
-      return Argumentos::Ajuda;
-   } else if total == 2 {
+   if total == 1 
+      { Argumentos::Ajuda } 
+   else if total == 2 {
       // para melhorar legibilidade.
       let opcao = argumentos[1].clone();
       // um argumento em terminal(apenas opção).
       if argumentos[1] == "info" 
-         { return Argumentos::Infomarcao; }
+         { Argumentos::Infomarcao }
       else if argumentos[1] == "procura" 
-         { return Argumentos::Procura; }
+         { Argumentos::Procura }
       else if argumentos[1] == "ajuda" 
-         { return Argumentos::Ajuda; }
+         { Argumentos::Ajuda }
       // opções privadas, inacessíveis ao usuário.
       else if opcao == "inverte-última-inserção" 
-         { return Argumentos::Privado(Funcao::Inversao); }
+         { Argumentos::Privado(Funcao::Inversao) }
       // continuação das opções "públicas".
       else if argumentos[1] == "backup"
-         { return Argumentos::Backup; }
-      else { return Argumentos::Ajuda; }
+         { Argumentos::Backup }
+      else 
+         { Argumentos::Ajuda }
    } else if total == 3 {
       // para melhorar legibilidade.
       let opcao = &argumentos[1];
@@ -167,42 +163,43 @@ pub fn transforma(argumentos:&Vec<String>) -> Argumentos {
       // dois argumentos(opção + argumento):
       if opcao == "tempo" && texto_de_tempo {
          let valor = argumento_em_tempo(arg.as_str()).unwrap();
-         return Argumentos::ProcuraTempo(valor.as_secs_f32());
+         Argumentos::ProcuraTempo(valor.as_secs_f32())
       } else if opcao == "quantia" && valor_numerico {
          let valor:u64 = arg.parse().unwrap();
-         return Argumentos::ProcuraQtd(valor);
+         Argumentos::ProcuraQtd(valor)
       } else if opcao  == "ignição" { 
-         return Argumentos::Privado(Funcao::Chamada);
+         Argumentos::Privado(Funcao::Chamada)
       } else if opcao == "varre" {
-         return Argumentos::Privado(Funcao::Processo);
+         Argumentos::Privado(Funcao::Processo)
       } else if opcao == "processa" {
-         return Argumentos::Privado(Funcao::PoderTotal);
+         Argumentos::Privado(Funcao::PoderTotal)
       } else if opcao == "nova-ignição" {
-         return Argumentos::Privado(Funcao::NovaChamada);
+         Argumentos::Privado(Funcao::NovaChamada)
       } else 
-         { return Argumentos::Ajuda; }
+         { Argumentos::Ajuda }
    } else if total == 4 {
       let opcao = &argumentos[1];
       if opcao == "função-deleta-caminho" {
-         return Argumentos::Privado(Funcao::Deleta); 
+         Argumentos::Privado(Funcao::Deleta)
       } else 
-         { return Argumentos::Ajuda; }
+         { Argumentos::Ajuda }
    }else { 
       // três ou mais argumentos, ainda não implementado...
       println!("só um argumento permitido por vez!");
-      return Argumentos::Ajuda;
+      Argumentos::Ajuda
    }
 }
 
-/// partindo do presuposto que está neste formato...
+/// Partindo do presuposto que está neste formato...
+#[allow(clippy::suspicious_else_formatting)]
 fn argumento_em_tempo(string:&str) -> Result<Duration, Error> {
    let e_hora = {
-      string.contains("h") ||
+      string.contains('h') ||
       string.contains("hora")
    };
    let e_minuto = {
       string.contains("min") ||
-      string.contains("m") ||
+      string.contains('m') ||
       string.contains("minuto")
    };
    
@@ -267,5 +264,4 @@ mod tests {
       assert!(!argumento_valido("28segs"));
       assert!(!argumento_valido("37mins"));
    }
-
 }
